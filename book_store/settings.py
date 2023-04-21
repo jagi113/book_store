617e7ef71c43ecdfa22d17047fb5cfafa16e823a
jagi113 <jaroslavgirovsky@gmail.com>
2023-04-21 16:32:23 +0200

    cart added

--- book_store/settings.py
+++ book_store/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "books.apps.BooksConfig",
+    "mathfilters",
 ]
 
 MIDDLEWARE = [
--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, HttpResponseRedirect, redirect
+from django.views.generic import ListView, DetailView, View
+from django.urls import reverse
 import operator
 
 from books.models import Book
@@ -12,6 +13,13 @@ class Index(ListView):
     template_name = "index.html"
     model = Book
     context_object_name = "books"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        request = self.request
+        cart = request.session.get("cart")
+        context["cart"] = cart
+        return context
 
     def get_queryset(self):
         queryset = {
@@ -27,8 +35,16 @@ class Book_detail(DetailView):
     model = Book
     context_object_name = "book"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        request = self.request
+        cart = request.session.get("cart")
+        context["cart"] = cart
+        return context
+
 
 def list_books(request):
+    cart = request.session.get("cart")
     books, search_query = searchBooks(request)
 
     order = "title"
@@ -43,5 +59,45 @@ def list_books(request):
         "books": book_list,
         "search_query": search_query,
         "custom_range": custom_range,
+        "cart": cart,
     }
     return render(request, "books/products.html", context)
+
+
+def add_to_cart(request):
+    try:
+        url = request.META.get("HTTP_REFERER")
+    except:
+        url = None
+
+    if request.method == "POST":
+        book_id = request.POST["buy_id"]
+        cart = request.session.get("cart")
+        if cart:
+            request.session["cart"] = cart.append(book_id)
+        else:
+            request.session["cart"] = [
+                book_id,
+            ]
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/"))
+
+
+def cart_summary(request):
+    cart = request.session.get("cart")
+    print(cart)
+    if not cart:
+        return render(request, "books/cart_summary.html", {cart: None})
+    purchasing_books = [Book.objects.get(pk=book) for book in cart]
+
+    price = round(float(sum([book.price for book in purchasing_books])), 2)
+    no_tax_price = round(
+        sum([(float(book.price) / 1.2) for book in purchasing_books]), 2
+    )
+    tax = round(price - no_tax_price, 2)
+    context = {
+        "cart": purchasing_books,
+        "no_tax_price": no_tax_price,
+        "tax": tax,
+        "price": price,
+    }
+    return render(request, "books/cart_summary.html", context)
